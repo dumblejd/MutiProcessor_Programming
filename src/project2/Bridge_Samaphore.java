07877e593a75ff6f5605ee99210f4a01bd11dec8
Di Jin <332548128@qq.com>
2018-10-05 16:48:19 -0500

    samephore complete

--- src/project2/Bridge_Samaphore.java
+++ src/project2/Bridge_Samaphore.java
@@ -1,79 +1,152 @@
 package project2;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Bridge_Samaphore {
-	Semaphore bridge = new Semaphore(1);
-	Semaphore back = new Semaphore(1);
-	Semaphore forth = new Semaphore(1);
-	private int num_back=0;
-	private int num_forth=0;
-	public void arriveBridge(int direction)
+	volatile int []waiting = new int[2];
+	volatile int []active = new int[2];
+	Semaphore []direction;
+	Semaphore mutex;;
+	Bridge_Samaphore(){
+		waiting[0]=0;
+		waiting[1]=0;
+		active[0]=0;
+		active[1]=0;
+		direction=new Semaphore[] {new Semaphore(0),new Semaphore(0)};
+		mutex = new Semaphore(1);
+	}
+	public int reverse(int myDirection)
 	{
-		if(direction==0)//back
+		return 1-myDirection;
+	}
+	public void arriveBridge(int myDirection) throws InterruptedException
+	{
+		mutex.acquire();
+		if(active[reverse(myDirection)]>0||waiting[reverse(myDirection)]>0)
+		{ //need to wait
+			waiting[myDirection]++;
+			mutex.release();
+			direction[myDirection].acquire();
+			System.out.println(Thread.currentThread().getId()+" has arrived bridge  direction: "+myDirection+" opposite waiting:"+waiting[reverse(myDirection)]+"  active:"+active[reverse(myDirection)]+"  myside waiting:"+waiting[myDirection]+"  active:"+active[myDirection]);
+		}
+			
+		else 
 		{
+			active[myDirection]++;
+			System.out.println(Thread.currentThread().getId()+" has arrived bridge  direction: "+myDirection+" opposite waiting:"+waiting[reverse(myDirection)]+"  active:"+active[reverse(myDirection)]+"  myside waiting:"+waiting[myDirection]+"  active:"+active[myDirection]);
+			mutex.release();
+		}
+		
+	}
+	public void leaveBridge(int myDirection) throws InterruptedException
+	{
+		mutex.acquire();
+		if(active[myDirection]-1==0 && waiting[reverse(myDirection)]>0)
+		{
+			active[myDirection]--;
+			System.out.println(Thread.currentThread().getId()+" has leave bridge  direction: "+myDirection+" opposite waiting:"+waiting[reverse(myDirection)]+"  active:"+active[reverse(myDirection)]+"  myside: waiting"+waiting[myDirection]+"  active:"+active[myDirection]);
+			direction[reverse(myDirection)].release(); //if there is one waiting on opposite direction, let it go
+			waiting[reverse(myDirection)]--;
+			active[reverse(myDirection)]++;
+		}
+		else if(active[myDirection]-1==0 && waiting[myDirection]>0)
+		{ //if no cars on opposite direction, let all cars in my direction go
+			active[myDirection]--;
+			System.out.println(Thread.currentThread().getId()+" has leave bridge  direction: "+myDirection+" opposite waiting:"+waiting[reverse(myDirection)]+"  active:"+active[reverse(myDirection)]+"  myside: waiting"+waiting[myDirection]+"  active:"+active[myDirection]);
+			while(waiting[myDirection]>0)
+			{
+				direction[myDirection].release();
+				active[myDirection]++;
+				waiting[myDirection]--;
+			}
+		}
+		else
+		{
+			active[myDirection]--;
+			System.out.println(Thread.currentThread().getId()+" has leave bridge  direction: "+myDirection+" opposite waiting:"+waiting[reverse(myDirection)]+"  active:"+active[reverse(myDirection)]+"  myside: waiting"+waiting[myDirection]+"  active:"+active[myDirection]);
+		}
+		
+		mutex.release();
+	}
+	static class myThread implements Runnable
+	{
+		private int direction;
+		Bridge_Samaphore bridge;
+		public myThread(int direction,Bridge_Samaphore bridge)   //1 represent go  0 represent come
+		{
+			this.direction=direction;
+			this.bridge = bridge;
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
 			try {
-				back.acquire();
-				if(num_forth>0)
-				{
-					bridge.acquire();
-				}
-				num_back++;
-				back.release();
-				//===========cs
-				back.acquire();
-				num_back--;
-				if(num_forth>0)
-				{
-					bridge.release();
-				}
-				back.release();
+				bridge.arriveBridge(direction);
+				
+				bridge.leaveBridge(direction);
 				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		else //forth
-		{
-			try {
-				forth.acquire();
-				num_forth++;
-				if(num_back>0)
-				{
-					back.acquire();
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		}
 		
 	}
-	public void leaveBridge(int direction)
+	public static void test(int num)
 	{
-		if(direction==0)//back
+		Bridge_Samaphore bridge = new Bridge_Samaphore();
+		Thread[] t = new Thread[num];
+		int dir=0;
+		for(int i=0;i<num;i++)
+		{
+			t[i]=new Thread(new myThread(1-dir,bridge));
+			dir=1-dir;
+		}
+		for(int i=0;i<num;i++)
+		{
+			t[i].start();
+		}
+		for(int i=0;i<num;i++)
 		{
 			try {
-				num_back--;
-				back.release();
-				if(num_forth>0)
-				{
-					
-				}
+				t[i].join();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		else //forth
+	}
+	public static void testOneSide(int num)
+	{
+		Bridge_Samaphore bridge = new Bridge_Samaphore();
+		Thread[] t = new Thread[num];
+	
+		for(int i=0;i<num;i++)
 		{
-			
+			t[i]=new Thread(new myThread(1,bridge));
+		
+		}
+		t[num/3]=new Thread(new myThread(0,bridge));
+		for(int i=0;i<num;i++)
+		{
+			t[i].start();
+		}
+		for(int i=0;i<num;i++)
+		{
+			try {
+				t[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		test(20);
+		//testOneSide(10);
 	}
 
 }
---
+++ src/project2/Bridige_Monitor.java
@@ -0,0 +1,19 @@
+package project2;
+
+public class Bridige_Monitor {
+	volatile int []waiting = new int[2];
+	volatile int []active = new int[2];
+	public synchronized void arriveBridge()
+	{
+		
+	}
+	public synchronized void leaveBridge()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
